36d54a23109bfb74611c118aed02923db6171d5f
roshni-guha <rguha9@wisc.edu>
2025-06-10 22:50:03 +0400

    Testing financials and historical data

--- stock_prices.py
+++ stock_prices.py
@@ -7,9 +7,25 @@ ticker_symbol1 = "AAPL"
 ticker1 = yfin.Ticker(ticker_symbol1)
 
 #Fetching historicaal market data
-history_data = ticker1.history(period = "5y") #data for past 5 years
+history_data = ticker1.history(period = "6mo") #data for past 5 years
 print("historical data:")
 print(history_data)
+
+#Fetching basic financials
+financials = ticker1.financials
+print("\nFinancials:")
+print(financials)
+
+#Fetching info about dividends and splits
+dividends = ticker1.dividends
+print("\nDividends:")
+print(dividends)
+splits = ticker1.splits
+print("\nSplits:")
+print(splits)
+
+
+
 
 
 
