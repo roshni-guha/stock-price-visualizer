874f5001397957e2bb71e06d7463f65c51afb9e9
roshni-guha <rguha9@wisc.edu>
2025-06-11 20:56:21 +0400

    Adding a cursor

--- stock_prices.py
+++ stock_prices.py
@@ -1,9 +1,8 @@
 import yfinance as yfin
 import matplotlib.pyplot as plt
 import matplotlib.animation
-from matplotlib.animation import FuncAnimation
 import numpy as np
-import matplotlib.animation as animation
+import mplcursors
 
 #Fetching historicaal market data
 history_data = yfin.download("AAPL", start="2020-01-01", end="2025-05-01", interval="1d")
@@ -16,11 +15,14 @@ history_data['SMA'] = history_data['Close'].rolling(window=20).mean()  # 20-day 
 print(history_data["SMA"].tail(100))
 
 APPLE = history_data['SMA']
-fig1 = plt.plot(APPLE, label='AAPL', color='blue', linewidth=2)
+fig, ax = plt.subplots()
+line, = ax.plot(history_data.index, APPLE, label='AAPL', color='blue', linewidth=2)  # Capture line and use fig/ax
 plt.legend(loc='upper left')
 plt.title("20 day Simple Moving Average")
 plt.xlabel("Date")
 plt.ylabel("Price")
+cursor = mplcursors.cursor(line, hover=True) #Cursor for interactive data point display
+
 plt.show()
 
 
