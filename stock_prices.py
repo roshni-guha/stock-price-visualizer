ecaabe5f75cb1d345857ee545cb993f4ac8322f6
roshni-guha <rguha9@wisc.edu>
2025-06-11 21:25:14 +0400

    Adding MSFT

--- stock_prices.py
+++ stock_prices.py
@@ -5,29 +5,36 @@ import numpy as np
 import mplcursors
 
 #Fetching historicaal market data
-history_data = yfin.download("AAPL", start="2020-01-01", end="2025-05-01", interval="1d")
+history_data_AAPL = yfin.download("AAPL", start="2020-01-01", end="2025-05-01", interval="1d")
+history_data_MSFT = yfin.download("MSFT", start="2020-01-01", end="2025-05-01", interval="1d")
 
-print("historical data:")
-print(history_data)
+print("historical data of Apple:")
+print(history_data_AAPL)
+print("historical data of Microsoft:")
+print(history_data_MSFT)
 
 #Simple Moving Average (SMA) 20-day calculation 
-history_data['SMA'] = history_data['Close'].rolling(window=20).mean()  # 20-day Simple Moving Average
-print(history_data["SMA"].tail(100))
+history_data_AAPL['SMA'] = history_data_AAPL['Close'].rolling(window=20).mean()  # 20-day Simple Moving Average of Apple
+history_data_MSFT['SMA'] = history_data_MSFT['Close'].rolling(window=20).mean()  # 20-day Simple Moving Average of Microsoft
+print(history_data_AAPL["SMA"].tail(100))
+print(history_data_MSFT["SMA"].tail(100))
 
-APPLE = history_data['SMA']
+APPLE = history_data_AAPL['SMA']
+MSFT = history_data_MSFT['SMA']
 fig, ax = plt.subplots()
-line, = ax.plot(history_data.index, APPLE, label='AAPL', color='blue', linewidth=2)  # Capture line and use fig/ax
-plt.legend(loc='upper left')
+line, = ax.plot(history_data_AAPL.index, APPLE, label='AAPL', color='blue', linewidth=2)  # Capture line and use fig/ax
+
 plt.title("20 day Simple Moving Average")
 plt.xlabel("Date")
 plt.ylabel("Price")
-cursor = mplcursors.cursor(line, hover=True) #Cursor for interactive data point display
+cursor = mplcursors.cursor(line, hover=False) #Cursor for interactive data point display
 
+
+line2, = ax.plot(history_data_MSFT.index, MSFT, label='MSFT', color='orange', linewidth=2)  # Capture line2
+cursor2 = mplcursors.cursor(line2, hover=True)  # Cursor for interactive data point display
+
+plt.legend(loc='upper left')
 plt.show()
-
-
-
-
 
 
 
